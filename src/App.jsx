02fbe8875975198194963c6634e19f3261da4d6e
vsaikrishnarao <vennamanenisaikrishnarao@gmail.com>
2026-04-19 18:37:28 -0500

    deleted redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Navbar from './components/Navbar'
-// import BoardListPage from './pages/BoardListPage'
-// import BoardPage from './pages/BoardPage'
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<BoardListPage />} />
-//         <Route path="/boards/:boardId" element={<BoardPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import BoardListPage from './pages/BoardListPage'
