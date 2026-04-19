05449b42244fe8cc99b37f8f35725439bba3b11a
vsaikrishnarao <vennamanenisaikrishnarao@gmail.com>
2026-04-19 18:31:56 -0500

    added login page with register option

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,4 +7,12 @@ const api = axios.create({
   }
 })
 
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  } else {
+    delete api.defaults.headers.common['Authorization']
+  }
+}
+
 export default api
